6cebdf97d605641bea019283a56828e3ac29a1f8
yogesh1801 <114429499+yogesh1801@users.noreply.github.com>
2024-07-05 09:33:21 +0530

    assignment

---
+++ Code/elasticsearch.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import os
+import json
+from elasticsearch7 import Elasticsearch
+from elasticsearch7.helpers import bulk
+
+def process(input_file, output_dir):
+    df = pd.read_csv(input_file, usecols=range(10))
+    df['id'] = range(1, len(df) + 1)
+    
+    columns_to_fill = ['title', 'tag', 'sourceURL', 'category']
+    df[columns_to_fill] = df[columns_to_fill].fillna('')
+
+    es = Elasticsearch(['http://localhost:9200'])
+    documents = []
+
+    INDEX_NAME = "assignment_index"
+    if not es.indices.exists(index=INDEX_NAME):
+        es.indices.create(index=INDEX_NAME)
+        print(f"Created index: {INDEX_NAME}")
+
+    for _, row in df.iterrows():
+
+        document = {
+            "_index": INDEX_NAME,
+            "_id": int(row['id']),
+            "title": row['title'],
+            "tags": row['tag'],
+            "url": row['sourceURL'],
+            "category": row['category']
+        }
+        documents.append(document)
+        
+    success, _ = bulk(es, documents)
+    print(f"Indexed {success} documents")
+
+    queries_file = "../queries.txt"
+    with open(queries_file, 'r') as f:
+        queries = f.readlines()
+
+    results = {}
+
+    for i, query in enumerate(queries, 1):
+        query = query.strip().split('. ', 1)[-1].strip()
+        res = es.search(index=INDEX_NAME,
+            query = {
+                "multi_match": {
+                    "query": query,
+                    "fields": ["title", "tags", "category", "url"],
+                    "type": "best_fields",
+                    "tie_breaker": 0.3
+                }
+            },
+            size= 100)
+
+        results[f"query_{i}"] = res['hits']['hits']
+
+    print("queries done")
+    
+    os.makedirs(output_dir, exist_ok=True)
+    for query_num, docs in results.items():
+        with open(f"{output_dir}/query{query_num}Results.txt", 'w', encoding='utf-8') as f:
+            for doc in docs:
+                doc_id = doc['_id']
+                title = doc['_source']['title']
+                score = doc['_score']
+                f.write(f"{doc_id} {title} {score}\n")
+    print("Results written")
+    
+if __name__ == "__main__":
+    input_file = "../assessment_data.csv"  
+    output_dir = "../Results"  
+    process(input_file, output_dir)
+    print("Processing completed successfully.")
---
+++ README.md
@@ -0,0 +1,58 @@
+# Elasticsearch Document Processing and Querying
+
+This project processes a CSV file, indexes the data in Elasticsearch, and performs queries based on a given set of queries.
+
+## Prerequisites
+
+- Python 3.7+
+- Docker and Docker Compose
+- Git (for cloning the repository)
+
+## File structure
+```
+project_root/
+├── Code/
+│   └── elasticsearch.py
+├── assessment_data.csv
+├── queries.txt              
+├── results/                
+└── docker-compose.yml 
+```
+
+## Setup
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/yogesh1801/stealth-pulse.git
+    cd stealth-pulse
+    ```
+2. Create and activate a virtual environment(windows) - you will venv package to do so:
+    ```sh
+    python -m venv venv
+    source venv/Scripts/activate
+    ```
+3. Install required packages
+    ```sh
+    pip install -r requirements.txt
+    ```
+4. start the ES and Kibana contatiners
+    ```sh
+    docker compose up -d
+    ```
+5. Run the python script
+    ```sh
+    cd Code
+    python elasticsearch.py
+    ```
+
+## Cleaning up  
+
+1. Stop the containers
+    ```sh
+    docker-compose down
+    ```
+
+2. Deactivate the virtual environment
+    ```sh
+    deactivate
+    ```
